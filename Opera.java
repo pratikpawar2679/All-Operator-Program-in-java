0e027bcac1358d522a401386dc8e4e951d013399
Pratik Pawar <111550955+pratikpawar2679@users.noreply.github.com>
2022-12-09 18:08:23 -1100

    Add files via upload

---
+++ Opera.java
@@ -0,0 +1,66 @@
+
+// Arithmetic operator
+// public class Opera {
+// public static void main(String[] args) {
+
+// int a = 2, b = 2;
+// System.out.println("a+b=" + (a + b));
+// System.out.println("a-b=" + (a - b));
+// System.out.println("a*b=" + (a * b));
+// System.out.println("a/b=" + (a / b));
+// System.out.println("a%b=" + (a % b));
+// }
+// }
+
+// Unary operator
+/**
+ * Opera
+ * //
+ */
+// public class Opera {
+
+// public static void main(String[] args) {
+// // pre increment
+// // int a = 1;
+// // int b = ++a;
+// // System.out.println(a);// 2
+// // System.out.println(b);// 2
+
+// // pre drecement
+// // int a = 1;
+// // int b = --a;
+// // System.out.println(a);// 0
+// // System.out.println(b);// 0
+
+// // post increment
+// // // pre increment
+// // int a = 1;
+// // int b = a++;
+// // System.out.println(a);// 2
+// // System.out.println(b);// 1
+
+// // post drecrement
+// // int a = 1;
+// // int b = a--;
+// // System.out.println(a);// 2
+// // System.out.println(b);// 1
+
+// }
+// }
+
+// Realational operator
+/**
+ * Opera
+ */
+// public class Opera {
+
+// public static void main(String[] args) {
+// int a = 2, b = 3;
+// System.out.println(a == b);// f
+// System.out.println(a != b);// t
+// System.out.println(a > b);// f
+// System.out.println(a < b);// t
+// System.out.println(a >= b);// f
+// System.out.println(a <= b);// t
+// }
+// }
